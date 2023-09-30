cc540e2a8e646272b1fe682c6f30cdfae4cc76b5
odogwukelly <odogwukellyice@gmail.com>
2023-09-30 11:59:57 +0000

    Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
-  const task2 = true;
+  let task2 = true;
 
   if (trueOrFalse) {
     task = true;
+    task2 = false;
   }
 
   return [task, task2];
